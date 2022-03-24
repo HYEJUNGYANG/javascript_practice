de07ba9309b785abedbb1545efc1e583ec1a0d78
HyeJung <gpwjd248@gmail.com>
2022-03-24 22:32:30 +0900

    variable: declaration and assignment

---
+++ 1. variable/1.variable.js
@@ -0,0 +1,11 @@
+// 변수 선언과 값의 할당(Variable declaration and assignment)
+let a = 0;
+console.log(a);
+
+// 변수 선언(Variable declaration)
+let b;
+console.log(b);
+
+// 값의 재할당(value reassigning)
+b = 2;
+console.log(b);
