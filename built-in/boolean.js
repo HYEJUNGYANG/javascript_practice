0f3877baa9e53a1a3a5df687a97a762c2b396fe6
HyeJung <gpwjd248@gmail.com>
2022-03-27 03:50:05 +0900

    built-in: boolean

---
+++ built-in/boolean.js
@@ -0,0 +1,23 @@
+// Boolean
+const isTrue = true; // new Boolean(true);
+console.log(isTrue.valueOf());
+
+/*
+[ False인 것들 ]
+ - 0 
+ - -0
+ - null
+ - NaN
+ - undefined
+ - ''
+*/
+
+/*
+[ True인 것들 ]
+ - 1
+ - -1
+ - '0'
+ - 'false'
+ - []
+ - {}
+*/
