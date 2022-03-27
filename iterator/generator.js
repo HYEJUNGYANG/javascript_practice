a21e77d449827c7d71dde7440ea9f96b415cbc59
HyeJung <gpwjd248@gmail.com>
2022-03-28 06:47:16 +0900

    iterator: generator

---
+++ iterator/generator.js
@@ -0,0 +1,31 @@
+// const multiple = {
+//     [Symbol.iterator]: () => {
+//         const max = 10;
+//         let num = 0;
+//         return {
+//             next() {
+//                 return { value: num++ * 2, done: num > max };
+//             },
+//         };
+//     },
+// };
+
+function* multipleGenerator() {
+    try {
+        for (let i = 0; i < 10; i++) {
+            console.log(i);
+            yield i ** 2; // next를 호출해야 순회됨
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+const multiple = multipleGenerator();
+let next = multiple.next();
+console.log(next.value, next.done);
+
+// multiple.return();
+multiple.throw('Error!');
+
+next = multiple.next();
+console.log(next.value, next.done);
