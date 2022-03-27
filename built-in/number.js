ff4cfdac4a900d3f8a5ef7b9ba2aebb9b346819d
HyeJung <gpwjd248@gmail.com>
2022-03-27 17:19:31 +0900

    built-in: number

---
+++ built-in/number.js
@@ -0,0 +1,46 @@
+const num1 = 123;
+const num2 = new Number(123); // 굳이 필요한게 아니라면 메모리 낭비가 될 수도 있음
+console.log(typeof num1);
+console.log(typeof num2);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.NaN);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.POSITIVE_INFINITY);
+
+if (num1 < Number.MAX_VALUE) {
+}
+if (Number.isNaN(num1)) {
+}
+
+// 지수표기법 (매우 크거나 작은 숫자를 표기할 때 사용, 10의 n승으로 표기)
+const num3 = 102;
+console.log(num3.toExponential()); // 1.02e+2 -> 1.02 * 10^2
+
+// 반올림하여 문자열로 변환
+const num4 = 1234.12;
+console.log(num4.toFixed());
+
+console.log(num4.toString());
+console.log(num4.toLocaleString('ar-EG'));
+
+// 원하는 자릿수까지 유효하도록 반올림
+console.log(num4.toPrecision(5));
+console.log(num4.toPrecision(4));
+console.log(num4.toPrecision(2)); // 전체 자릿수 표기가 안될때는 지수표기법으로
+
+if (Number.EPSILON > 0 || Number.EPSILON < 1) {
+    console.log(Number.EPSILON); // 0과 1사이에서 나타낼 수 있는 가장 작은 숫자
+}
+const num = 0.1 + 0.2 - 0.2; // 0.1
+console.log(num);
+
+function isEqual(original, expected) {
+    return Math.abs(original - expected) < Number.EPSILON;
+}
+console.log(isEqual(1, 1));
+console.log(isEqual(0.1, 0.1));
+console.log(isEqual(num, 0.1));
