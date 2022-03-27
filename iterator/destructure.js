57d3f29e10a45362a10f3aa19a395824afb1898f
HyeJung <gpwjd248@gmail.com>
2022-03-28 07:41:22 +0900

    iterator: destructure-quiz

--- iterator/destructure.js
+++ iterator/destructure.js
@@ -32,3 +32,20 @@ console.log(name);
 console.log(age);
 console.log(occupation);
 console.log(pet);
+
+// Quiz
+const prop = {
+    name: 'Button',
+
+    styles: {
+        size: 20,
+
+        color: 'black',
+    },
+};
+
+function changeColor({ styles: { color } }) {
+    console.log(color);
+}
+
+changeColor(prop);
