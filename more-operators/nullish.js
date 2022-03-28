fbf65dc6f6c1adc41584eb4e633337c84115fa93
HyeJung <gpwjd248@gmail.com>
2022-03-29 01:40:47 +0900

    more-operators: nullish

---
+++ more-operators/nullish.js
@@ -0,0 +1,7 @@
+// Nullish coalescing Operator
+// ES11 (ECMAScript 2020)
+// ?? null, undefined
+// || falshy한 경우 설정(할당) 0, -0, null, undefined, ''
+let num = 0;
+console.log(num || '-1');
+console.log(num ?? '-1');
