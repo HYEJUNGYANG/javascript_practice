a29b7f1e9206e7f49b76b771d3ab3f15e02c7d5f
HyeJung <gpwjd248@gmail.com>
2022-03-29 23:44:36 +0900

    async: quiz1

---
+++ async/quiz1.js
@@ -0,0 +1,31 @@
+// 주어진 seconds(초)가 지나면 callback함수를 호출함
+// 단, seconds가 0보다 작으면 에러를 던지자! ⏱
+// function runInDelay(callback, seconds) {
+//     if (seconds < 0) {
+//         throw new Error('error');
+//     }
+//     const milliSec = seconds * 1000;
+//     setTimeout(callback, milliSec);
+// }
+
+// runInDelay(() => {
+//     console.log('test');
+// }, 3);
+
+function runInDelay(callback, seconds) {
+    if (!callback) {
+        throw new Error('callback함수를 전달 해야 함');
+    }
+    if (!seconds || seconds < 0) {
+        throw new Error('seconds는 0보다 커야 함');
+    }
+    setTimeout(callback, seconds * 1000);
+}
+
+try {
+    runInDelay(() => {
+        console.log('타이머 완료!');
+    }, -1);
+} catch (error) {
+    console.log(error);
+}
