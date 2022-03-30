04a8625989c6958eb5ec4a8776a3a0060e8cbc06
HyeJung <gpwjd248@gmail.com>
2022-03-31 00:00:48 +0900

    scope: scope-quiz

--- scope/scope-quiz.js
+++ scope/scope-quiz.js
@@ -8,9 +8,9 @@
     // console.log(y);
 }
 
-const text = 'global';
+const text = 'global';  // 전역 변수, 전역 스코프 (글로벌 변수, 글로벌 스코프)
 {
-    const text = 'inside block1';
+    const text = 'inside block1';  // 지역 변수(로컬 변수), 지역 스코프(로컬 스코프)
     {
         const text = 'inside block2';
         console.log(text);
