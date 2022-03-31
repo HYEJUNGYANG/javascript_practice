0a230655e96dc2fd60ddae9494eb8e38a4a8dd9a
HyeJung <gpwjd248@gmail.com>
2022-04-01 02:39:37 +0900

    closure: var

---
+++ closure/var.js
@@ -0,0 +1,18 @@
+function loop() {
+    const array = [];
+    // var와 let의 차이점!
+    for (var i = 0; i < 5; i++) {
+        array.push(function () {
+            console.log(i);
+        });
+    }
+    // for (let i = 0; i < 5; i++) {
+    //     array.push(function () {
+    //         console.log(i);
+    //     });
+    // }
+    return array;
+}
+
+const array = loop();
+array.forEach((func) => func());
