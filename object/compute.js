5da8be17fbf5a2ae86cb04990c193a0c45f319fa
양혜정 <gpwjd248@gmail.com>
2022-05-24 11:36:01 +0900

    add result

--- object/compute.js
+++ object/compute.js
@@ -1,6 +1,6 @@
 const obj = {
-    name: '혜정',
-    age: 23,
+  name: '혜정',
+  age: 23
 };
 // 코딩하는 시점에, 정적으로 접근이 확정됨
 obj.name;
@@ -8,17 +8,17 @@ obj.age;
 
 // 동적으로 속성에 접근하고 싶을때 대괄호 표기법 사용!
 function getValue(obj, key) {
-    // return obj.key; // ❌
-    return obj[key];
+  //   return obj.key; // ❌ undefined
+  return obj[key];
 }
 console.log(getValue(obj, 'name'));
 
 function addKey(obj, key, value) {
-    obj[key] = value;
+  obj[key] = value;
 }
 addKey(obj, 'job', 'engineer');
 console.log(obj);
 
 function deleteKey(obj, key) {
-    delete obj[key];
+  delete obj[key];
 }
